836e3080a15a4c09dd6ba038a46d79f5a1b5f467
jewelism <boseokjung@gmail.com>
2018-07-21 15:13:11 +0900

    add shell script log

--- back.sh
+++ back.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo "boseok_log_server build"
+echo "boseok_log_server auto build start"
 eval "cd /var/www/boseok_log_server && sudo git pull && sudo pm2 restart all"
+echo "boseok_log_server auto build end"
--- front.sh
+++ front.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo "boseok_log build"
+echo "boseok_log auto build start"
 eval "cd /var/www/boseok_log && sudo git pull && sudo npm run build && sudo service nginx restart"
+echo "boseok_log auto build end"
