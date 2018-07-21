c41f26bc8fe1e68d666e65e5551b2d73159d5fcf
jewelism <boseokjung@gmail.com>
2018-07-21 15:06:04 +0900

    fix url

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const shell = require('shelljs');
 
 http.createServer((req, res) => {
   console.log('building...');
-  res.write('<span>building...</span>');
-  req.url === 'front' ? shell.exec('./front.sh') : shell.exec('./back.sh');
+  const {url} = req;
+  url === '/front' ? shell.exec('./front.sh') : shell.exec('./back.sh');
+  res.write(`building ${url}`);
   res.end();
 }).listen(8887);
