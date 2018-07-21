74a76a5bc03b1b2ca9113a53f930573cc0f6e3a0
jewelism <boseokjung@gmail.com>
2018-07-21 15:01:45 +0900

    add log

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require('http');
 const shell = require('shelljs');
 
 http.createServer((req, res) => {
+  console.log('building...');
+  req.write('<span>building...</span>');
   req.url === 'front' ? shell.exec('./front.sh') : shell.exec('./back.sh');
   res.end();
 }).listen(8887);
